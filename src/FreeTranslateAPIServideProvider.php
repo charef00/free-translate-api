a23ad8fe76ca889f9f64fb50dfdb79f7ad5b1eb7
ayoub <ayouubcharef@gmail.com>
2022-06-16 00:19:00 +0100

    create service provider

---
+++ src/FreeTranslateAPIServideProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Charef\FreeTranslateApi;
+
+use Illuminate\Support\ServiceProvider;
+
+class FreeTranslateAPIServideProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        include __DIR__.'/routes.php';
+    }
+}
---
+++ src/routes.php
@@ -0,0 +1,6 @@
+<?php 
+
+Route::get('tst',function()
+{
+    echo "free translate api";
+});
